2f9a713d3cc1b0657bc993ce41d02cb277f7a82f
shanike <56911506+shanike@users.noreply.github.com>
2024-06-03 16:08:58 +0300

    set funcs

--- mmn22/set.c
+++ mmn22/set.c
@@ -1,40 +1,102 @@
+#include <stdio.h>
 #include "set.h"
 
-/* Override the set `set_name` with `elements` */
-void read_set(char *set_name, int *elements)
+void print_ints_until_negative(int *arr)
 {
-    /* TODO */
+    int i = -1;
+    while (*(arr + (++i)) != -1)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+void print_string(char *arr)
+{
+    int i = -1;
+    while (*(arr + (++i)) != '\0')
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+/* UTIL: Reset set.elements to 0 */
+void reset_set(SET *set)
+{
+    int i;
+    for (i = 0; i < NUM_OF_BYTES; i++)
+    {
+        (*set).elements[i] = 0;
+    }
+}
+
+/*
+Override the set set set`ith `elements`,
+where `elements` is an array of integers,
+where the last element is -1.
+*/
+void read_set(SET *set, int *elements)
+{
+    int i = -1;
+    int elem, byte_index, bit_index;
+
+    reset_set(set);
+
+    /* Register elements into set */
+    while ((elem = elements[++i]) != -1)
+    {
+        byte_index = elements[i] / BYTE_SIZE;
+        bit_index = elements[i] % BYTE_SIZE;
+        (*set).elements[byte_index] |= 1 << bit_index;
+    }
 }
 
 /*
 Pretty prints in an ascending order with a max of 16 values per line.
 If set is empty prints "The set is empty"
 */
-void print_set(char *set_name)
+void print_set(SET *set)
 {
-    /* TODO */
+    int is_empty = 0;
+    int byte_i, bit_j;
+    for (byte_i = 0; byte_i < NUM_OF_BYTES; byte_i++)
+    {
+        for (bit_j = 0; bit_j < BYTE_SIZE; bit_j++)
+        {
+            if (((*set).elements[byte_i] & (1 << bit_j)))
+            {
+                if (is_empty == 0)
+                {
+                    printf("{ ");
+                }
+                is_empty = 1;
+                printf("%d ", byte_i * BYTE_SIZE + bit_j);
+            }
+        }
+    }
+    printf("}\n");
 }
 
 /* Unites sets 1 and 2 into set 3 */
-void union_set(char *set_name1, char *set_name2, char *set_name3)
+void union_set(SET set1, SET set2, SET set3)
 {
     /* TODO */
 }
 
 /* Intersects sets 1 and 2 into set 3 */
-void intersect_set(char *set_name1, char *set_name2, char *set_name3)
+void intersect_set(SET set1, SET set2, SET set3)
 {
     /* TODO */
 }
 
 /* Subtracts set 2 from 1 into set 3 */
-void sub_set(char *set_name1, char *set_name2, char *set_name3)
+void sub_set(SET set1, SET set2, SET set3)
 {
     /* TODO */
 }
 
 /* Symetric diff between set 1 and 2 into set 3 */
-void symdiff_set(char *set_name1, char *set_name2, char *set_name3)
+void symdiff_set(SET set1, SET set2, SET set3)
 {
     /* TODO */
 }
--- mmn22/set.h
+++ mmn22/set.h
@@ -1,11 +1,20 @@
 /* Set values vary between 0 and 127 */
-#define MAX 128
+#define MAX_NUMBER 128
+#define BYTE_SIZE 8
+#define NUM_OF_BYTES MAX_NUMBER / BYTE_SIZE
 
-typedef char set[MAX];
+/* An array of 8 characters, representing 128 digits */
+typedef char ELEMENTS[NUM_OF_BYTES];
 
-void read_set(char *set_name, int *elements);
-void print_set(char *set_name);
-void union_set(char *set_name1, char *set_name2, char *set_name3);
-void intersect_set(char *set_name1, char *set_name2, char *set_name3);
-void sub_set(char *set_name1, char *set_name2, char *set_name3);
-void symdiff_set(char *set_name1, char *set_name2, char *set_name3);
+typedef struct set
+{
+    ELEMENTS elements;
+    int is_empty;
+} SET;
+
+void read_set(SET *, int[]);
+void print_set(SET *);
+void union_set(SET, SET, SET);
+void intersect_set(SET, SET, SET);
+void sub_set(SET, SET, SET);
+void symdiff_set(SET, SET, SET);
