87757ccf55e15805e68292c2094c570871bae0df
Reut Avital <reut.avital1@gmail.com>
2024-07-30 21:29:31 +0300

    error handling files finished (pre assembler part)

---
+++ mmn14/error_handling.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
+#include "./generic_file_functions.h"
+#include "./error_handling.h"
+
+/* The Following array specifies the error status code numbers and the corresponding error message. */
+Error errors[] = {
+    {ERROR_STATUS_CODE_100, "Failed to dynamically allocate memory"},
+    {ERROR_STATUS_CODE_101, "Failed to open new file for writing"},
+    {ERROR_STATUS_CODE_102, "Failed to open file for reading"},
+    {ERROR_STATUS_CODE_103, "Error - macro without name definition"},
+    {ERROR_STATUS_CODE_104, "Error - extra text after macro name definition"},
+    {ERROR_STATUS_CODE_105, "Setting a position in a file failed"},
+    {ERROR_STATUS_CODE_106, "'endmcro' with extra text"},
+    {ERROR_STATUS_CODE_107, "Failed to copy file during macros expansion"},
+    {ERROR_STATUS_CODE_108, "Macros expansion in an .as file failed"},
+    {ERROR_STATUS_CODE_109, "Illegal name for a macro"},
+    {ERROR_STATUS_CODE_110, "Line is too long"},
+};
+
+void print_system_error(int error_code)
+{
+    printf("SYSTEM ERROR: CODE:%d | %s\n", error_code, errors[error_code].error_msg);
+}
+
+void print_file_error(int error_code, location_in_file file)
+{
+    printf("FILE ERROR: CODE:%d in %s at line:%d | there is error: %s\n", error_code,
+           file.file_name, file.line_number, errors[error_code].error_msg);
+}
---
+++ mmn14/error_handling.h
@@ -0,0 +1,52 @@
+#ifndef ERROR_HANDLING_H
+#define ERROR_HANDLING_H
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "global_variables.h"
+#include "generic_file_functions.h"
+
+
+/* An error with an associated error code and error message */
+typedef struct Error
+{
+    int error_code;    /* Unique code for the error */
+    char *error_msg; /* Error message associated with the error */
+} Error;
+
+typedef enum ERROR_STATUS_CODES
+{
+    ERROR_STATUS_CODE_100, /* Allocate memory failure */
+    ERROR_STATUS_CODE_101, /* Failed open file for writing */
+    ERROR_STATUS_CODE_102, /* Failed open file for reading */
+    ERROR_STATUS_CODE_103, /* Macro without name definition */
+    ERROR_STATUS_CODE_104, /* Extra text after macro name definition*/
+    ERROR_STATUS_CODE_105, /* Failed to set a position in a file */
+    ERROR_STATUS_CODE_106, /* Extra text in "endmcro" line */
+    ERROR_STATUS_CODE_107, /* Failed to copy file during macros expansion */
+    ERROR_STATUS_CODE_108, /* Macros expansion in an .as file failed */
+    ERROR_STATUS_CODE_109, /* Illegal name for macro */
+    ERROR_STATUS_CODE_110 /* Line is too long */
+} ERROR_STATUS_CODES;
+
+/* Function declarations */
+
+/**
+ * Print a system error message to the standard output.
+ * 
+ * @param error_code The error code for which the message should be printed.
+ */
+void print_system_error(int error_code);
+
+/**
+ * Print a file-related error message to the standard output.
+ * 
+ * @param error_code The error code for which the message should be printed.
+ * @param file       The file location where the error occurred.
+ */
+void print_file_error(int error_code, location_in_file file);
+
+
+#endif /* ERROR_HANDLING_H */
