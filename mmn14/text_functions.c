ba7485788ec2f087f8c6ce2d1f2e23d76e04fb17
Reut Avital <reut.avital1@gmail.com>
2024-07-30 21:28:48 +0300

    text-functions file finished

---
+++ mmn14/text_functions.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+void remove_spaces_next_to_comma(char *str)
+{
+    /* New pointer to the str*/
+    char *ptr = str;
+
+    if (*ptr == ',')
+        return;
+
+    while ((ptr = strchr(ptr, ',')) != NULL)
+    {
+        /* Check if there is a space before the comma */
+        if (ptr > str && *(ptr - 1) == ' ')
+        {
+            /* Remove the space before the comma */
+            memmove(ptr - 1, ptr, strlen(ptr) + 1);
+            /* After removing the space, check if there is also a space right after the comma */
+            if (*(ptr) == ' ')
+            {
+                /* Remove the space after the comma */
+                memmove(ptr, ptr + 1, strlen(ptr));
+            }
+        }
+        /* Check if there is a space after the comma (and not already handled above) */
+        else if (*(ptr + 1) == ' ')
+        {
+            memmove(ptr + 1, ptr + 2, strlen(ptr + 2) + 1); /* Remove the space after the comma */
+        }
+
+        /* Move to the next character after the comma */
+        ptr++;
+    }
+}
+
+/* Check if the char is a space or a tab */
+int is_space_or_tab(char c)
+{
+    return (isspace(c) && c != '\n');
+}
---
+++ mmn14/text_functions.h
@@ -0,0 +1,25 @@
+#ifndef TEXT_FUNCTIONS_H
+#define TEXT_FUNCTIONS_H
+
+/**
+ * Removes spaces adjacent to commas in a string.
+ * 
+ * This function iterates through the string, and for each comma, it removes
+ * any spaces that are immediately before or after the comma.
+ * 
+ * @param str The string to process.
+ */
+void remove_spaces_next_to_comma(char *str);
+
+/**
+ * Checks if a character is a space or a tab.
+ * 
+ * This function determines if the given character is a space (' ') or a tab ('\t').
+ * It excludes newline characters ('\n') from being considered as spaces or tabs.
+ * 
+ * @param c The character to check.
+ * @return 1 if the character is a space or a tab, 0 otherwise.
+ */
+int is_space_or_tab(char c);
+
+#endif /* TEXT_FUNCTIONS_H */
