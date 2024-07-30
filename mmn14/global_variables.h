fa4386e85411ce359758d6ec2d1b6564a1449a87
Reut Avital <reut.avital1@gmail.com>
2024-07-30 21:30:38 +0300

    global variables file (pre assembler part)

---
+++ mmn14/global_variables.h
@@ -0,0 +1,25 @@
+#ifndef GLOBAL_VARIABLES_H
+#define GLOBAL_VARIABLES_H
+
+/* Macro declarations */
+#define MACRO_DECLARATION "mcro"
+#define MACRO_END "endmcro"
+
+/* Maximum length of a single command line */
+#define MAX_LINE_LENGTH 81
+
+/* Success and failure codes */
+#define SUCCESS 1
+#define FAILURE 0
+
+
+#define INSTRUCTIONS_NUMBER 4
+#define OP_CODES_NUMBER 16
+#define REGISTERS_NUMBER 8
+
+typedef struct location_in_file {
+    char *file_name;
+    int line_number;
+} location_in_file;
+
+#endif /* GLOBAL_VARIABLES_H */
