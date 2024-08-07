431244d774d0fdc986869c7aed6325234b39271d
Shani Kehati <56911506+shanike@users.noreply.github.com>
2024-08-07 09:20:01 +0300

    init first pass

---
+++ mmn14/first_pass.c
@@ -0,0 +1,12 @@
+/* *BTW I got the phrase "first pass" from google translate and chatgpt */
+#include "./first_pass.h"
+
+int first_pass(char filename[])
+{
+    /* Data counter == מונה הנתונים */
+    int DC = 0;
+    /* Instructions counter == מונה ההוראות */
+    int IC = 0;
+    
+    return 0;
+}
---
+++ mmn14/first_pass.h
@@ -0,0 +1,2 @@
+/* Main function of first_pass */
+int first_pass(char filename[]);
