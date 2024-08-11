308813d18f672d708729ef128540b30e214025cf
Shani Kehati <56911506+shanike@users.noreply.github.com>
2024-08-11 21:50:38 +0300

    (assembler)

--- mmn14/assembler.c
+++ mmn14/assembler.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
     if (argc < 2)
     {
         fprintf(stderr, "Usage: %s <file1> <file2> ... <fileN>\n", argv[0]);
-        return 0;
+        return 1;
     }
 
     for (i = 1; i < argc; i++)
@@ -22,45 +22,21 @@ int main(int argc, char *argv[])
         input_file = argv[i];
 
         printf("Processing file: %s\n", input_file);
-        
+
         result = process_macros(create_new_file(input_file, ".as"));
 
-        if (result == 1)
-        {
-            printf("Pre-assembly completed successfully for file: %s\n", input_file);
-        }
-        else
+        if (!result)
         {
             fprintf(stderr, "Pre-assembly failed for file: %s\n", input_file);
         }
 
         result = first_pass(create_new_file(input_file, ".am"));
 
-        if (result == 0)
-        {
-            printf("First pass completed successfully for file: %s\n", input_file);
-        }
-        else
+        if (!result)
         {
             fprintf(stderr, "First pass failed for file: %s\n", input_file);
         }
-
     }
 
-    return 1;
-
-    /*
-        int i;
-        if (argc < 2)
-        {
-            fprintf(stderr, "Error: No input file(s)\n");
-            printf("Usage: assembler <file1> <file2> ...\n");
-            return 1;
-        }
-        for (i = 1; i < argc; i++)
-        {
-            process_macros(argv[i]);
-        }
-        return 0;
-    */
+    return 0;
 }
