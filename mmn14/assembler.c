8a4be1c842c2e1d5ad452928bfdd7ee83d46f827
Shani Kehati <56911506+shanike@users.noreply.github.com>
2024-08-11 18:30:10 +0300

    clean assembler

--- mmn14/assembler.c
+++ mmn14/assembler.c
@@ -5,22 +5,11 @@
 #include "./first_pass.h"
 #include "./generic_file_functions.h"
 
-int temp_first_pass(char input_filename[])
-{
-    char *am_filename = create_new_file(input_filename, ".am");
-    printf("TEMP - Calling first_pass with .am (=after pre-assembler) file: %s\n", am_filename);
-    if (first_pass(am_filename) == FAILURE)
-    {
-        fprintf(stderr, "Error: first pass failed for file: %s\n", am_filename);
-        return 1;
-    }
-    return 0;
-}
-
 int main(int argc, char *argv[])
 {
     int result;
     int i;
+    char *input_file;
 
     if (argc < 2)
     {
@@ -30,10 +19,10 @@ int main(int argc, char *argv[])
 
     for (i = 1; i < argc; i++)
     {
-        char *input_file = argv[i];
+        input_file = argv[i];
 
         printf("Processing file: %s\n", input_file);
-        return temp_first_pass(input_file); /* TODO remove this line and the temp function*/
+        
         result = process_macros(create_new_file(input_file, ".as"));
 
         if (result == 1)
