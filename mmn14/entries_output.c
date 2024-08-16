ee5c50463398a022cf3e6b3c4d0a1006c2262ad7
Shani Kehati <56911506+shanike@users.noreply.github.com>
2024-08-16 11:36:23 +0300

    fix(.ent): no file if no entry label

--- mmn14/entries_output.c
+++ mmn14/entries_output.c
@@ -4,29 +4,30 @@
 int create_entries_output(labelNode *labels_table, char *filename)
 {
     labelNode *current_label = labels_table;
-    char *entries_filename = create_new_file(filename, ENTRY_FILE_EXT);
-    FILE *entries_fp;
+    char *entries_filename = NULL;
+    FILE *entries_fp = NULL;
 
+    /* Set entries filename */
+    entries_filename = create_new_file(filename, ENTRY_FILE_EXT);
     if (entries_filename == NULL)
     {
         return FAILURE;
     }
 
-    if (!open_file_for_writing(entries_filename, &entries_fp))
-    {
-        return FAILURE;
-    }
-    printf("here\n");
+    /* Find entries and fill file */
     while (current_label)
     {
         if (current_label->feature_type == ENTRY)
         {
+            soft_open_file_for_writing(entries_filename, &entries_fp);
             fprintf(entries_fp, "%s %03d\n", current_label->name, current_label->value);
         }
         current_label = current_label->next;
     }
 
-    fclose(entries_fp);
+    soft_fclose(&entries_fp);
+
+    free(entries_filename);
 
     return SUCCESS;
 }
