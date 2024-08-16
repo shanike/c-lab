e229876285238a9b00ba213db5f562bebe453bee
Shani Kehati <56911506+shanike@users.noreply.github.com>
2024-08-16 18:35:16 +0300

    turned DEBUG flags off

--- mmn14/global_variables.h
+++ mmn14/global_variables.h
@@ -1,10 +1,10 @@
 #ifndef GLOBAL_VARIABLES_H
 #define GLOBAL_VARIABLES_H
 
-#define IS_DEBUG 1
+#define IS_DEBUG 0
 #define IS_DEBUG_FIRST_PASS 0
 #define IS_DEBUG_ENCODING 0
-#define IS_DEBUG_SECOND_PASS 1
+#define IS_DEBUG_SECOND_PASS 0
 
 /* Macro declarations */
 #define MACRO_DECLARATION "macr"
