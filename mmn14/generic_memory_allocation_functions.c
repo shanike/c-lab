df1931730777afa5850e6732616b5db5c02dc8ae
Shani Kehati <56911506+shanike@users.noreply.github.com>
2024-08-18 13:53:23 +0300

    refactor(allocate_memory_with_check): exit() on failure

--- mmn14/generic_memory_allocation_functions.c
+++ mmn14/generic_memory_allocation_functions.c
@@ -1,7 +1,9 @@
 #include "generic_memory_allocation_functions.h"
 
-/* TODO exit() program */
-/* Allocates memory and checks if the allocation was successful */
+/*
+Allocates memory and checks if the allocation was successful
+If the allocation fails, the function prints an error message and exits the program
+*/
 void *allocate_memory_with_check(size_t size)
 {
     void *ptr = malloc(size);
@@ -9,6 +11,7 @@ void *allocate_memory_with_check(size_t size)
     {
         /* Report the memory allocation failure */
         print_system_error(ERROR_STATUS_CODE_100);
+        exit(EXIT_FAILURE);
     }
     return ptr;
 }
