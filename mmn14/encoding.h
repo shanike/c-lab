d445208305801822fa7f0b764a4d8bd68a31f348
Shani Kehati <56911506+shanike@users.noreply.github.com>
2024-08-18 15:50:38 +0300

    style+docs(encoding)

--- mmn14/encoding.h
+++ mmn14/encoding.h
@@ -14,13 +14,25 @@
 #define ARGS_DELIM ", \t" /* Delimiters for the arguments of an instruction */
 
 /* Function to encode an operation */
-int encode_instruction(operation *op, char *args_str, location_in_file file_location, int *IC, wordNode **instructions_table, labelNode *labels_list);
+int encode_instruction(operation *op,
+                       char *args_str,
+                       location_in_file file_location,
+                       int *IC,
+                       wordNode **instructions_table,
+                       labelNode *labels_list);
 
 /*
-Encodes the labels of an instruction line and adds external labels to the external file.
-And fully updates the IC.
+Encodes the labels of an instruction line and adds external labels to the external list.
+Even though only the labels are encoded, the IC updates fully.
+
 Important to notice that if the instruction line got to this function, most validations on it have already been done.
 */
-int encode_labels(operation *op, char *args_str, location_in_file file_location, int *IC, wordNode **instructions_table, labelNode *labels_list, labelNode **externals_table);
+int encode_labels(operation *op,
+                  char *args_str,
+                  location_in_file file_location,
+                  int *IC,
+                  wordNode **instructions_table,
+                  labelNode *labels_list,
+                  labelNode **externals_table);
 
 #endif
