34ec22c8675e328d4931064bc89e72dd3ee09ea6
Shani Kehati <56911506+shanike@users.noreply.github.com>
2024-08-18 18:20:15 +0300

    refactor: replace malloc with allocate_mem_with_check

--- mmn14/text_functions.c
+++ mmn14/text_functions.c
@@ -67,7 +67,11 @@ int extract_data_string(char *input, char **result, location_in_file curr_locati
     result_len = second_quote_index - 1;
 
     /* Copy the input without the quotes */
-    *result = malloc(result_len + 1);
+    *result = allocate_memory_with_check(result_len + 1);
+    if (!(*result))
+    {
+        return FAILURE;
+    }
     strncpy(*result, input + 1, result_len);
     (*result)[result_len] = '\0';
 
