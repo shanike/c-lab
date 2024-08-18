66d55026fa4a478c41dca11ec4a2bec79e7bdb07
Shani Kehati <56911506+shanike@users.noreply.github.com>
2024-08-18 16:02:02 +0300

    refactor(entries)

--- mmn14/entries_output.c
+++ mmn14/entries_output.c
@@ -1,34 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "entries_output.h"
 
-int create_entries_file(labelNode *labels_table, char *filename)
+void create_entries_file(labelNode *labels_table, char *filename)
 {
     labelNode *current_label = labels_table;
     char *entries_filename = NULL;
     FILE *entries_fp = NULL;
 
+    if (current_label == NULL)
+    {
+        return;
+    }
+
     /* Set entries filename */
     entries_filename = create_new_file_name(filename, ENTRY_FILE_EXT);
     if (entries_filename == NULL)
     {
-        return FAILURE;
+        return;
     }
 
-    /* Find entries and fill file */
+    if (open_file_for_writing(entries_filename, &entries_fp) == FAILURE)
+    {
+        free(entries_filename);
+        return;
+    }
+
+    /* Find entry labels and fill file */
     while (current_label)
     {
         if (current_label->feature_type == ENTRY)
         {
-            soft_open_file_for_writing(entries_filename, &entries_fp);
             fprintf(entries_fp, "%s %03d\n", current_label->name, current_label->value);
         }
         current_label = current_label->next;
     }
 
-    soft_fclose(&entries_fp);
-
+    fclose(entries_fp);
     free(entries_filename);
-
-    return SUCCESS;
 }
